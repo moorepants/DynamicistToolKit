6d93e6055cbc76d97b190a164fbdea294632d95e
Jason Moore <moorepants@gmail.com>
2011-06-22 12:56:54 -0700

    making a setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Inertia',
+      version='0.1dev',
+      packages=['inertia',],
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('README.md').read(),
+      )
