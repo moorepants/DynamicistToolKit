00ca5c3bdc48c5bcaa543ce444a1246f2da0c2d8
Jason K. Moore <moorepants@gmail.com>
2013-08-06 14:57:32 -0400

    v0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='DynamicistToolKit',
       author='Jason Moore',
       author_email='moorepants@gmail.com',
-      version='0.1.0dev',
+      version='0.1.0',
       packages=['dtk'],
       license='Creative Commons Attribution-Noncommercial-Share Alike license',
       long_description=open('README.rst').read(),
