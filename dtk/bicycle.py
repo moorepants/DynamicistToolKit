8c79ab8d75408651ddb233abce7bab419cee42f0
Jason K. Moore <moorepants@gmail.com>
2024-06-11 21:07:07 +0200

    Fix sphinx build errors due to no latex with mpl.

--- dtk/bicycle.py
+++ dtk/bicycle.py
@@ -344,13 +344,12 @@ def meijaard_figure_four(time, rollRate, steerRate, speed):
     height = width * golden_ratio
     fig = figure()
     fig.set_size_inches([width, height])
-    params = {'backend': 'ps',
-              'axes.labelsize': 10,
-              #'text.fontsize': 10,
-              'legend.fontsize': 10,
-              'xtick.labelsize': 8,
-              'ytick.labelsize': 8,
-              'text.usetex': True}
+    params = {
+        'axes.labelsize': 10,
+        'legend.fontsize': 10,
+        'xtick.labelsize': 8,
+        'ytick.labelsize': 8,
+    }
     rcParams.update(params)
 
     fig.subplots_adjust(right=0.85, left=0.15, bottom=0.15)
