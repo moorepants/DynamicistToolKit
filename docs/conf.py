53b0fa13afc7dc9a999cc275d5264af4ecc8a31a
Jason K. Moore <moorepants@gmail.com>
2024-06-14 07:36:38 +0200

    Updated copyright to include all authors.

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DynamicistToolKit'
-copyright = u'2011-2024, Jason K. Moore'
+copyright = u'2011-2024, DynamicistToolKit Authors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
