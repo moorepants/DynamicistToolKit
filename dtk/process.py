3c53ffcbb19c658009a478c2f464605454feb8f1
Jason K. Moore <moorepants@gmail.com>
2024-06-14 07:06:59 +0200

    Added remaining examples to process except for least squares variance (not sure about that one).

--- dtk/process.py
+++ dtk/process.py
@@ -4,7 +4,7 @@
 # external dependencies
 import numpy as np
 from numpy.fft import fft, fftfreq
-from scipy.integrate import trapz, cumtrapz
+from scipy.integrate import trapezoid, cumulative_trapezoid
 from scipy.interpolate import UnivariateSpline
 from scipy.optimize import fmin
 from scipy.signal import butter, sosfiltfilt
@@ -306,6 +306,18 @@ def coefficient_of_determination(measured, predicted):
     r_squared : float
        The coefficient of determination.
 
+    Examples
+    -------
+
+    >>> import numpy as np
+    >>> from dtk.process import coefficient_of_determination
+    >>> np.random.seed(10)
+    >>> t = np.linspace(0.0, 10.0, num=1001)
+    >>> predicted = np.sin(t)
+    >>> measured = predicted + np.random.normal(0.01, 0.1, size=len(t))
+    >>> coefficient_of_determination(measured, predicted)
+    0.980225686442542
+
     Notes
     -----
 
@@ -326,10 +338,12 @@ def coefficient_of_determination(measured, predicted):
     """
     # 2-norm => np.sqrt(np.sum(measured - predicted)**2))
 
-    numerator = np.linalg.norm(measured - predicted) ** 2
-    denominator = np.linalg.norm(measured - measured.mean()) ** 2
+    numerator = np.linalg.norm(measured - predicted)**2
+    denominator = np.linalg.norm(measured - measured.mean())**2
 
-    r_squared = 1.0 - numerator / denominator
+    r_squared = 1.0 - numerator/denominator
+
+    # TODO : Does not give the same r^2 as fit_goodness below.
 
     return r_squared
 
@@ -356,6 +370,18 @@ def fit_goodness(ym, yp):
     SSR : float
         The regression sum of squares.
 
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from dtk.process import fit_goodness
+    >>> np.random.seed(10)
+    >>> t = np.linspace(0.0, 10.0, num=1001)
+    >>> predicted = np.sin(t)
+    >>> measured = predicted + np.random.normal(0.01, 0.1, size=len(t))
+    >>> fit_goodness(measured, predicted)
+    (0.9862246380225299, 6.197685397793293, 449.9108922095648, 443.7132068117715)
+
     Notes
     -----
 
@@ -364,10 +390,10 @@ def fit_goodness(ym, yp):
     '''
 
     ym_bar = np.mean(ym)
-    SSR = sum((yp - ym_bar) ** 2)
-    SST = sum((ym - ym_bar) ** 2)
+    SSR = sum((yp - ym_bar)**2)
+    SST = sum((ym - ym_bar)**2)
     SSE = SST - SSR
-    rsq = SSR / SST
+    rsq = SSR/SST
 
     return rsq, SSE, SST, SSR
 
@@ -452,25 +478,40 @@ def curve_area_stats(x, y):
     Returns
     -------
     A dictionary containing:
-    median : ndarray, shape (m,)
-        The x value corresponding to 0.5*area under the curve
-    lq : ndarray, shape (m,)
-        lower quartile
-    uq : ndarray, shape (m,)
-        upper quartile
-    98p : ndarray, shape (m,)
-        98th percentile
-    2p : ndarray, shape (m,)
-        2nd percentile
+       median : ndarray, shape (m,)
+           The x value corresponding to 0.5*area under the curve
+       lq : ndarray, shape (m,)
+           lower quartile
+       uq : ndarray, shape (m,)
+           upper quartile
+       98p : ndarray, shape (m,)
+           98th percentile
+       2p : ndarray, shape (m,)
+           2nd percentile
+
+    Examples
+    --------
+
+    >>> from pprint import pprint
+    >>> import numpy as np
+    >>> from dtk.process import curve_area_stats
+    >>> x = np.linspace(0.0, 10.0, num=1001)
+    >>> y = np.vstack((np.exp(x), 0.5*x)).T
+    >>> pprint(curve_area_stats(x, y))
+    {'2p': array([6.09, 1.41]),
+     '98p': array([9.97, 9.89]),
+     'lq': array([8.61, 5.  ]),
+     'median': array([9.3 , 7.07]),
+     'uq': array([9.71, 8.66])}
 
     '''
-    area = trapz(y, x=x, axis=0)  # shape (m,)
+    area = trapezoid(y, x=x, axis=0)  # shape (m,)
     percents = np.array([0.02*area,
                          0.25*area,
                          0.5*area,
                          0.75*area,
                          0.98*area])  # shape (5,m)
-    CumArea = cumtrapz(y.T, x=x.T)  # shape(m,n)
+    CumArea = cumulative_trapezoid(y.T, x=x.T)  # shape(m,n)
     xstats = {'2p': [], 'lq': [], 'median': [], 'uq': [], '98p': []}
     for j, curve in enumerate(CumArea):
         flags = [False for flag in range(5)]
@@ -932,6 +973,22 @@ def subtract_mean(sig, hasNans=False):
     ndarray, shape(n,)
         sig minus the mean of sig
 
+    Examples
+    --------
+
+    >>> import numpy as np
+    >>> from dtk.process import subtract_mean
+    >>> t = np.linspace(0.0, 2*np.pi, num=11)
+    >>> y1 = np.sin(t)
+    >>> y2 = np.sin(t) + 0.3
+    >>> print(np.allclose(y1, subtract_mean(y2)))
+    True
+    >>> y2[2:5] = np.nan
+    >>> subtract_mean(y2, hasNans=True)
+    array([ 0.31123729,  0.89902254,         nan,         nan,         nan,
+            0.31123729, -0.27654797, -0.63981923, -0.63981923, -0.27654797,
+            0.31123729])
+
     '''
     if hasNans:
         return sig - nanmean(sig)
@@ -953,6 +1010,13 @@ def normalize(sig, hasNans=False):
     -------
     normSig : ndarray, shape(n,)
         The signal normalized with respect to the maximum value.
+
+    >>> import numpy as np
+    >>> from dtk.process import normalize
+    >>> t = np.linspace(0.0, 2*np.pi, num=11)
+    >>> y = 5.0*np.sin(t)
+    >>> np.max(normalize(y))
+    1.0
 
     '''
     # TODO : This could be a try/except statement instead of an optional
@@ -997,6 +1061,25 @@ def derivative(x, y, method='forward', padding=None):
     -------
     dydx : ndarray, shape(n,) or shape(n-1,)
         for combination else shape(n-1,)
+
+    Examples
+    --------
+
+    .. plot::
+       :context: reset
+       :include-source:
+
+       import numpy as np
+       import matplotlib.pyplot as plt
+       from dtk.process import derivative
+
+       x = np.linspace(-10.0, 10.0, num=201)
+       y = x**2
+
+       fig, axes = plt.subplots(2, 1, layout='constrained')
+
+       axes[0].plot(x, y)
+       axes[1].plot(x, derivative(x, y, method='combination'))
 
     """
     x = np.asarray(x)
@@ -1091,6 +1174,22 @@ def time_vector(num_samples, sample_rate, start_time=0.0):
     -------
     time : ndarray, shape(numSamples,)
         Time vector starting at zero.
+
+    Examples
+    --------
+
+    >>> from dtk.process import time_vector
+    >>> time_vector(101, 100)
+    array([0.  , 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1 ,
+           0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2 , 0.21,
+           0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3 , 0.31, 0.32,
+           0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.4 , 0.41, 0.42, 0.43,
+           0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5 , 0.51, 0.52, 0.53, 0.54,
+           0.55, 0.56, 0.57, 0.58, 0.59, 0.6 , 0.61, 0.62, 0.63, 0.64, 0.65,
+           0.66, 0.67, 0.68, 0.69, 0.7 , 0.71, 0.72, 0.73, 0.74, 0.75, 0.76,
+           0.77, 0.78, 0.79, 0.8 , 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87,
+           0.88, 0.89, 0.9 , 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98,
+           0.99, 1.  ])
 
     '''
 
